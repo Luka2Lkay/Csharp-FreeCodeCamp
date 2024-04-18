e4d80d94085b28192304acf7131fc89d6ca64965
Luka <lukhanyo95@gmail.com>
2024-04-18 18:43:24 +0200

    working with variable data

---
+++ workWithVariableData/variableData/Program.cs
@@ -0,0 +1,29 @@
+﻿// Use the MinValue and MaxValue properties for each signed integral type
+//A signed type uses its bytes to represent an equal number of positive and negative numbers. 
+
+Console.Clear();
+Console.WriteLine("Signed integral types:");
+
+Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
+Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
+Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
+Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+
+// Use the MinValue and MaxValue properties for each unsigned integral type
+// An unsigned type uses its bytes to represent only positive numbers.
+
+Console.WriteLine();
+Console.WriteLine("Unsigned integral types:");
+
+Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
+Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
+Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
+Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
+
+// Use the MinValue and MaxValue properties for each signed float type
+
+Console.WriteLine("");
+Console.WriteLine("Floating point types:");
+Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
+Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
+Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
