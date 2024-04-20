1423d607dadbdf78dc0cbe078ef7a2c5ee88b90d
Luka <lukhanyo95@gmail.com>
2024-04-20 14:17:34 +0200

    Exercise - Discover padding and alignment

--- stringFormatting/Program.cs
+++ stringFormatting/Program.cs
@@ -50,3 +50,34 @@ string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} pri
 yourDiscount += $"A discount of {((price1 - salePrice)/price1):P2}!"; //inserted
 
 Console.WriteLine(yourDiscount);
+
+// Display the invoice number using string interpolation
+Console.WriteLine();
+Console.WriteLine("Display the invoice number using string interpolation");
+
+int invoiceNumber = 1201;
+decimal productShares = 25.4568m;
+decimal subtotal = 2750.00m;
+decimal taxPercentage = .15825m;
+decimal total = 3185.19m;
+
+Console.WriteLine($"Invoice Number: {invoiceNumber}");
+Console.WriteLine($"   Shares: {productShares:N3} Product");
+Console.WriteLine($"     Sub Total: {subtotal:C}");
+Console.WriteLine($"           Tax: {taxPercentage:P2}");
+Console.WriteLine($"     Total Billed: {total:C}");
+
+// Working with padded strings
+Console.WriteLine();
+Console.WriteLine("Working with padded strings");
+
+string paymentId = "769C";
+ string payeeName = "Mr. Stephen Ortega";
+ string paymentAmount = "$5,000.00";
+
+ var formattedLine = paymentId.PadRight(6);
+ formattedLine += payeeName.PadRight(24);
+ formattedLine += paymentAmount.PadLeft(10);
+
+ Console.WriteLine("1234567890123456789012345678901234567890");
+ Console.WriteLine(formattedLine);
